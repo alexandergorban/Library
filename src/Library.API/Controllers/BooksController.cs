25884609dfebced84cb7cfe3a86b1b468374e2b4
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-02-10 15:38:45 +0200

    Validation on [HttpPut("{id}")] Book

--- src/Library.API/Controllers/BooksController.cs
+++ src/Library.API/Controllers/BooksController.cs
@@ -70,7 +70,6 @@ namespace Library.API.Controllers
 
             if (!ModelState.IsValid)
             {
-                // return 422
                 return new UnprocessableEntityObjectResult(ModelState);
             }
 
@@ -121,6 +120,21 @@ namespace Library.API.Controllers
             if (book == null)
             {
                 return BadRequest();
+            }
+
+            if (book.Description == book.Title)
+            {
+                ModelState.AddModelError(nameof(BookForUpdateDto), "The provided description should be different from the title.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return new UnprocessableEntityObjectResult(ModelState);
+            }
+
+            if (!_libraryRepository.AuthorExists(authorId))
+            {
+                return NotFound();
             }
 
             var bookForAuthorFromRepo = _libraryRepository.GetBookForAuthor(authorId, id);
