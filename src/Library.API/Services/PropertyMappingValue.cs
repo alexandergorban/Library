a5eb90de19e57876be956bac9d7acf641e0093c4
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-02-11 22:33:38 +0200

    Creating a Property Mapping Service

---
+++ src/Library.API/Services/PropertyMappingValue.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.API.Services
+{
+    public class PropertyMappingValue
+    {
+        public IEnumerable<string> DestinationPropeties { get; private set; }
+        public bool Revert { get; private set; }
+
+        public PropertyMappingValue(IEnumerable<string> destinationPropeties, bool revert = false)
+        {
+            DestinationPropeties = destinationPropeties;
+            Revert = revert;
+        }
+    }
+}
