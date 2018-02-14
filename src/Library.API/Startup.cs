f027f47813c33fde9d84bc3a7c341ac230de40f3
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-02-14 11:40:38 +0200

    Camel-casing Dictionaries When Serializing to JSON

--- src/Library.API/Startup.cs
+++ src/Library.API/Startup.cs
@@ -17,6 +17,7 @@ using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.AspNetCore.Mvc.Routing;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Serialization;
 using NLog.Extensions.Logging;
 
 namespace Library.API
@@ -39,7 +40,11 @@ namespace Library.API
                 setupAction.ReturnHttpNotAcceptable = true;
                 setupAction.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
                 setupAction.InputFormatters.Add(new XmlDataContractSerializerInputFormatter());
-            });
+            })
+                .AddJsonOptions(options =>
+                {
+                    options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                });
 
             // register the DbContext on the container, getting the connection string from
             // appSettings (note: use this during development; in a production environment,
