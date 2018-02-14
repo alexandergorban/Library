71111149927baf66a8fea23f0d413c3258d51bc4
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-02-14 09:03:24 +0200

    Creating a Method to Shape Resources

---
+++ src/Library.API/Helpers/IEnumerableExtensions.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Library.API.Helpers
+{
+    public static class IEnumerableExtensions
+    {
+        public static IEnumerable<ExpandoObject> ShapeData<TSource>(this IEnumerable<TSource> source, string fields)
+        {
+            if (source == null)
+            {
+                throw new ArgumentException("source");
+            }
+
+            // create a list to hold our ExpandoObject
+            var expandoObjectList = new List<ExpandoObject>();
+            // create a list with PropertyInfo object on TSource
+            var propertyInfoList = new List<PropertyInfo>();
+
+            if (string.IsNullOrWhiteSpace(fields))
+            {
+                // all public properties should be in the ExpandoObject
+                var propertyInfos = typeof(TSource).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+                propertyInfoList.AddRange(propertyInfos);
+            }
+            else
+            {
+                // only the public properties that match the fields should be in the ExpandoObject
+                var fieldsAfterSplit = fields.Split(',');
+
+                foreach (var field in fieldsAfterSplit)
+                {
+                    var propertyName = field.Trim();
+
+                    var propertyInfo = typeof(TSource).GetProperty(propertyName,
+                        BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+                    if (propertyInfo == null)
+                    {
+                        throw new Exception($"Property {propertyName} wasn't found on {typeof(TSource)}");
+                    }
+
+                    propertyInfoList.Add(propertyInfo);
+                }
+            }
+
+            // run through the source objects
+            foreach (TSource sourceObject in source)
+            {
+                var dataShapedObject = new ExpandoObject();
+
+                foreach (var propertyInfo in propertyInfoList)
+                {
+                    var propertyValue = propertyInfo.GetValue(sourceObject);
+                    ((IDictionary<string, object>)dataShapedObject).Add(propertyInfo.Name, propertyValue);
+                }
+
+                expandoObjectList.Add(dataShapedObject);
+            }
+
+            return expandoObjectList;
+        }
+    }
+}
