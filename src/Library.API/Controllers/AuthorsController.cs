24d1d0a21ec8bf470516dfce57326fafca712dd8
Alexander Gorban <gorban.alexander.y@gmail.com>
2018-02-19 01:23:02 +0200

    Supporting the HEAD Method

--- src/Library.API/Controllers/AuthorsController.cs
+++ src/Library.API/Controllers/AuthorsController.cs
@@ -30,6 +30,7 @@ namespace Library.API.Controllers
         }
 
         [HttpGet(Name = "GetAuthors")]
+        [HttpHead]
         public IActionResult GetAuthors(AuthorsResourceParameters authorsResourceParameters,
             [FromHeader(Name = "Accept")] string mediaType)
         {
