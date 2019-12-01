aaf1e2c7eea377f785670ba2adf4fcdbd6d8119f
Alexander Gorban <gorban.alexander.y@gmail.com>
2019-12-01 19:46:51 +0200

    Improving Documentation with XML Comments and Data Annotations on Model Classes

--- src/Library.API/Models/AuthorForCreationDto.cs
+++ src/Library.API/Models/AuthorForCreationDto.cs
@@ -1,16 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Library.API.Models
 {
+    /// <summary>
+    /// An author First Name, Last Name, Date of birth, Genre, Books fields
+    /// </summary>
     public class AuthorForCreationDto
     {
+        /// <summary>
+        /// The first name for the author
+        /// </summary>
+        [Required]
+        [MaxLength(150)]
         public string FirstName { get; set; }
+
+        /// <summary>
+        /// The last name for the author
+        /// </summary>
+        [Required]
+        [MaxLength(150)]
         public string LastName { get; set; }
+
+        /// <summary>
+        /// The date of birth for the author
+        /// </summary>
         public DateTimeOffset DateOfBirth { get; set; }
+
+        /// <summary>
+        /// The genre for the author
+        /// </summary>
         public string Genre { get; set; }
+
+        /// <summary>
+        /// The list of books for the author
+        /// </summary>
         public ICollection<BookForCreationDto> Books { get; set; } = new List<BookForCreationDto>();
     }
 }
